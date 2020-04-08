0b106b65162ad165cb783c2d1360ec3064787d22
laure-delisle <laure.delisle@gmail.com>
2020-04-07 18:46:10 -0700

    extract viz helper functions

---
+++ project 1 - facial keypoint detection/utils.py
@@ -0,0 +1,71 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+def net_sample_output_for_viz(net, data_loader):
+    
+    net.eval()
+    
+    # iterate through the test dataset
+    for i, sample in enumerate(data_loader):
+        
+        # get sample data: images and ground truth keypoints
+        images = sample['image']
+        key_pts = sample['keypoints']
+
+        # convert images to FloatTensors
+        images = images.type(torch.FloatTensor)
+
+        # forward pass to get net output
+        output_pts = net(images)
+        
+        # reshape to batch_size x 68 x 2 pts
+        output_pts = output_pts.view(output_pts.size()[0], 68, -1)
+        
+        # break after first image is tested
+        if i==0:
+            net.train()
+            return images, output_pts, key_pts
+
+
+def show_all_keypoints(image, predicted_key_pts, gt_pts=None):
+    """Show image with predicted keypoints"""
+    # image is grayscale
+    plt.imshow(image, cmap='gray')
+    plt.scatter(predicted_key_pts[:, 0], predicted_key_pts[:, 1], s=20, marker='.', c='m')
+    # plot ground truth points as green pts
+    if gt_pts is not None:
+        plt.scatter(gt_pts[:, 0], gt_pts[:, 1], s=20, marker='.', c='g')
+    plt.show()
+
+
+# visualize the output (batch of 10 by default)
+def visualize_output(test_images, test_outputs, gt_pts=None, batch_size=10):
+
+    for i in range(batch_size):
+        plt.figure(figsize=(4*batch_size,4))
+        ax = plt.subplot(1, batch_size, i+1)
+
+        # un-transform the image data
+        image = test_images[i].data   # get the image from it's wrapper
+        image = image.numpy()   # convert to numpy array from a Tensor
+        image = np.transpose(image, (1, 2, 0))  # transpose to go from torch to numpy image
+
+        # un-transform the predicted key_pts data
+        predicted_key_pts = test_outputs[i].data
+        predicted_key_pts = predicted_key_pts.numpy()
+        # undo normalization of keypoints  
+        predicted_key_pts = predicted_key_pts*50.0+100
+        
+        # plot ground truth points for comparison, if they exist
+        ground_truth_pts = None
+        if gt_pts is not None:
+            ground_truth_pts = gt_pts[i]         
+            ground_truth_pts = ground_truth_pts*50.0+100
+        
+        # call show_all_keypoints
+        show_all_keypoints(np.squeeze(image), predicted_key_pts, ground_truth_pts)
+            
+        plt.axis('off')
+
+    plt.show()
